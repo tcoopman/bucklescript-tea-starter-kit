185afdf67239cfe1a56309fc7315749dc7f82256
Thomas Coopman <thomas.coopman@gmail.com>
2017-08-27 10:50:05 +0200

    updated versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import node_resolve from 'rollup-plugin-node-resolve';
 
 export default {
-    entry: './lib/es6/src/main.js',
-    format: 'iife',
-    dest: './release/main.js',
+    input: './lib/es6/src/main.js',
+    output: {
+        file: './release/main.js',
+        format: 'iife'
+    },
     plugins: [
         node_resolve({module: true, browser: true}),
     ],
-    moduleName: 'starter'
+    name: 'starter'
 }
