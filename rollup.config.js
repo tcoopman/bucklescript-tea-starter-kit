887450aeac9b63e6874fc233f71a1031d9942b5e
Thomas Coopman <thomas.coopman@gmail.com>
2017-10-29 10:06:20 +0100

    upgrade to bs-platform 2.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import node_resolve from 'rollup-plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 
 export default {
-    input: './lib/es6/src/main.js',
+    input: './lib/es6/src/Main.js',
     output: {
         file: './release/main.js',
         format: 'iife'
